e82960db0528e41e020c8d82db17e25aabf881b2
Audrey Spinazzé <spinazze31@hotmail.fr>
2024-07-04 14:59:27 +0200

    ajout des bulles en bas des images

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,62 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+let arrows = document.querySelectorAll(".arrow");
+let banners = document.querySelectorAll(".banner-img");
+
+arrows.forEach((images) => {
+  images.addEventListener("click", (e) => {
+    console.log(images);
+    const imgNext = e.target.id === "next" ? 1 : -1;
+    console.log(imgNext);
+  });
+});
+
+function Createdots(slides) {
+  let Dots = document.createElement("img");
+  Dots.setAttribute("class", "dot");
+  Dots.setAttribute("id", "dot" + i);
+  document.getElementById("insertdots").appendChild(Dots);
+}
+
+for (i = 0; i < slides.length; i++) {
+  slides[i] = Createdots();
+}
+
+let Dots = document.getElementById("dot0");
+Dots.classList.add("dot_selected");
+
+let newImg = document.createElement("img");
+let newtxt = document.createElement("p");
+newImg.setAttribute("src", "../assets/images/slideshow/slide2.jpg");
+newImg.setAttribute("class", "banner-img");
+
+document.getElementById("banner").appendChild(newImg);
+document.getElementById("banner").appendChild(newtxt);
+
+newImg = document.createElement("img");
+newImg.setAttribute("src", "../assets/images/slideshow/slide3.jpg");
+newImg.setAttribute("class", "banner-img");
+document.getElementById("banner").appendChild(newImg);
+
+newImg = document.createElement("img");
+newImg.setAttribute("src", "../assets/images/slideshow/slide4.png");
+newImg.setAttribute("class", "banner-img");
+document.getElementById("banner").appendChild(newImg);
